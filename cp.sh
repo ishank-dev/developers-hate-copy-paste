7d77b694da0fd27101df98a595ca15b0419b1ff1
Ishank Sharma <45262592+ishank-dev@users.noreply.github.com>
2020-03-07 19:08:33 +0530

    upload shell script

---
+++ cp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#!/bin/bash
+# valid=true
+xdotool key ctrl+c
+# count=1
+xdotool key ctrl+alt+t
+
+# while [ $valid ]
+# do
+sleep 1
+
+# echo $count
+#xdotool type Linux is love
+
+# if [ $count -eq 5 ];
+# then
+# break
+# fi
+xdotool key ctrl+shift+v
+xdotool key Return
+# ((count++))
+# done
+
+
